1427e0a3b2ba1270a677e36f5dfcc1057ee4b4b4
lisaatea <lisaatea@gmail.com>
2024-08-21 10:13:08 -0700

    update broswer router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sunrise-visitor-guide-frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
