88fc1cbea58fac1dfb996201b14070a30d0507e4
lisaatea <lisaatea@gmail.com>
2024-08-21 09:51:59 -0700

    update file path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sunrise-visitor-guide-frontend/",
+  base: "/sunrise-visitor-guide-frontend",
   server: {
     port: 3000,
   },
